8a60a5f513f4592b04c81e5c52b6c12286eab1bb
Jemekite <hamdi@hamdi.web.id>
2016-10-26 11:36:35 +0200

    add .xz archive

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ blocks=`qubitcoin-cli getinfo | grep blocks | cut -d " " -f 7 | cut -d "," -f 1`
 date=`date -u`
 date_fmt=`date -u +%Y%m%d`
 file="bootstrap.dat"
-file_7z="$file.$date_fmt.7z"
+file_xz="$file.$date_fmt.tar.xz"
 file_zip="$file.$date_fmt.zip"
 file_sha256="sha256.txt"
 header=`cat header.md`
@@ -13,12 +13,15 @@ footer=`cat footer.md`
 ./linearize-hashes.py linearize.cfg > hashlist.txt
 ./linearize-data.py linearize.cfg 
 touch $file_sha256
+tar cJf $file_xz $file
 zip $file_zip $file
-sha256sum $file $file_zip > $file_sha256
+sha256sum $file $file_xz $file_zip > $file_sha256
+size_xz=`ls -lh $file_xz | awk -F" " '{ print $5 }'`
 size_zip=`ls -lh $file_zip | awk -F" " '{ print $5 }'`
+url_xz=`curl --upload-file $file_xz https://transfer.sh/$file_xz`
 url_zip=`curl --upload-file $file_zip https://transfer.sh/$file_zip`
 url_sha256=`curl --upload-file $file_sha256 https://transfer.sh/$file_sha256`
-newLinks="Block $blocks: $date [zip]($url_zip) ($size_zip) [SHA256]($url_sha256)\n\n$prevLinks"
+newLinks="`Block $blocks: $date [xz]($url_xz) ($size_xz) [zip]($url_zip) ($size_zip) [SHA256]($url_sha256)`\n\n$prevLinks"
 echo -e "$newLinks" > links.md
 rm $file $file_zip $file_sha256 hashlist.txt
 echo -e "$header\n\n####For mainnet:\n\n$newLinks\n\n$footer" > README.md
